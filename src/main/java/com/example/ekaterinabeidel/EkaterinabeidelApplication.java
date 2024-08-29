1a13f122b8954485f1cc8e043042cb31d4706aa3
ekaterinabeidel <156689206+ekaterinabeidel@users.noreply.github.com>
2024-08-29 14:40:18 +0200

    init project

---
+++ src/main/java/com/example/ekaterinabeidel/EkaterinabeidelApplication.java
@@ -0,0 +1,13 @@
+package com.example.ekaterinabeidel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EkaterinabeidelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EkaterinabeidelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ekaterinabeidel/EkaterinabeidelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ekaterinabeidel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EkaterinabeidelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
