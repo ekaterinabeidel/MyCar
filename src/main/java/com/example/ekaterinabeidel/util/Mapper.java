a9f611ae941e54add4d43f28e988d7d58f3ba55d
ekaterinabeidel <156689206+ekaterinabeidel@users.noreply.github.com>
2024-08-31 21:02:05 +0200

    delete interface Mapper.java

--- src/main/java/com/example/ekaterinabeidel/util/Mapper.java
+++
@@ -1,6 +0,0 @@
-package com.example.ekaterinabeidel.util;
-
-public interface Mapper {
-    // TODO: обсудить, нужно ли выносить логику мапперов в интерфейс или оставить утилитарными классами
-    // TODO: обсудить, нужна ли папка util
-}
